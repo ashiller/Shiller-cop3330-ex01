f5cf71f1d0030d9cfa7202c431e45decf583b989
ashiller <89662655+ashiller@users.noreply.github.com>
2021-09-13 19:49:47 -0400

    Add files via upload

---
+++ ex03.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var scanner = new java.util.Scanner(System.in);
+        System.out.print("What is the quote? ");
+        var quote = scanner.nextLine();
+        System.out.println("Who said it? ");
+        var author = scanner.nextLine();
+        System.out.println(author + "says, " + quote);
+    }
+}
